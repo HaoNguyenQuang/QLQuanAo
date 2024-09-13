fea5e44d2393e102b3a8d6b258d48c642372a0cb
HaoNguyenQuang <hao14102002123@gmail.com>
2024-09-13 08:32:45 +0700

    first commit

---
+++ src/main/java/com/example/quanlybanquanao/QuanLyBanQuanAoApplication.java
@@ -0,0 +1,13 @@
+package com.example.quanlybanquanao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyBanQuanAoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanLyBanQuanAoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quanlybanquanao/QuanLyBanQuanAoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quanlybanquanao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLyBanQuanAoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
